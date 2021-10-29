06164d6af5d3843a185f5b385e81ff6878ba455b
HJ Lee <umtt0820@gmail.com>
2021-10-29 16:27:59 +0900

    Enhancing your Web API: Part 2
    https://localhost:/products/rate?ProductId=jenlooper-cactus&Rating=5
    jenlooper-cactus 의 Ratings 배열에 5 추가됨

--- ContosoCrafts.WebSite/Controllers/ProductsController.cs
+++ ContosoCrafts.WebSite/Controllers/ProductsController.cs
@@ -26,5 +26,16 @@ namespace ContosoCrafts.WebSite.Controllers
         {
             return ProductService.GetProducts();
         }
+
+        //[HttpPatch] "[FromBody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string ProductId,
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
     }
 }
